676bd207c85b11a4d3a92ffa218500a514267b1e
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-12-12 17:43:26 +0900

    refactor(view): create and implements BaseballGameView.kt

---
+++ src/main/kotlin/baseball/view/BaseballGameView.kt
@@ -0,0 +1,20 @@
+package baseball.view
+
+class BaseballGameView : ConsoleGameView {
+
+    override fun onInit() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPlay() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFinish(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroy() {
+        TODO("Not yet implemented")
+    }
+}
