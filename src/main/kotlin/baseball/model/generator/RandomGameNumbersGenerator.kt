a69fc7a65d9c05c4e3fa1a86076b6451a10849f4
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-12-13 17:32:54 +0900

    fix(model): generate() 함수 수정

--- src/main/kotlin/baseball/model/generator/RandomGameNumbersGenerator.kt
+++ src/main/kotlin/baseball/model/generator/RandomGameNumbersGenerator.kt
@@ -1,14 +1,15 @@
 package baseball.model.generator
 
+import baseball.model.data.GameNumbers
 import camp.nextstep.edu.missionutils.Randoms
 
 class RandomGameNumbersGenerator : GameNumberGenerator {
 
     override fun generate(): List<Int> {
         return Randoms.pickUniqueNumbersInRange(
-            NumberList.MIN_NUMBER,
-            NumberList.MAX_NUMBER,
-            NumberList.LENGTH,
+            GameNumbers.MIN_NUMBER,
+            GameNumbers.MAX_NUMBER,
+            GameNumbers.LENGTH,
         )
     }
 }
